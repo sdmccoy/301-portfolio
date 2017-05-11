0e64086b188815fff5e78819b8a740c43ff1df62
Scott <sdmccoy@gmail.com>
2017-05-10 17:36:27 -0700

    When you input data in to the rawData js file array it will be built as a new project in the object constructor function in the porject.js file

---
+++ scripts/project.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var projects = [];
+
+function Project (rawDataObj) {
+  this.url = rawDataObj.url;
+  this.image = rawDataObj.image;
+  this.title = rawDataObj.title;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.category = rawDataObj.category;
+  this.description = rawDataObj.description;
+}
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('section.template').clone();
+  $newProject.removeClass('template');
+  if (!this.publishedOn) {
+    $newProject.addClass('draft');
+  }
+  $newProject.attr('a', this.url);
+  $newProject.find('img').attr('href', this.image);
+  $newProject.find('h2:first').html(this.title);
+  $newProject.attr('data-category', this.category);
+  $newProject.find('h4 p').html(this.publishedOn);
+  $newProject.find('p:second').html(this.description);
+};
