8ad40caf0f35b1ae239e815f7297f152b8bd9d15
Scott <sdmccoy@gmail.com>
2017-05-16 21:57:26 -0700

    the death loop of reload was line 18 on mainView.js. It would click automatically, then run the function, then try and click again. I put a return on line 14 to end the loop

--- scripts/mainView.js
+++ scripts/mainView.js
@@ -11,18 +11,17 @@ featureView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function(){
     $('.tab-content').hide();
     let selection = $(this).data('content');
-    console.log(selection);
-    $(`#${selection}-content`).fadeIn(700);
+    return $(`#${selection}-content`).fadeIn(700);
   });
   // this loads the first tab of the main nav on page load.
-  // $('.main-nav li.tab:first').click();
+  //SOMETHING HERE IS GOING INTO A LOOP AND CONTINUOUSLY RELOADING THE PAGE.
+  $('main-nav .tab:first').click();
 }
 
 featureView.initIndexPage = function() {
   Project.all.forEach(function(project){
     $('#projects-content').append(project.toHtml());
   });
-  //SOMETHING HERE IS GOING INTO A LOOP AND CONTINUOUSLY RELOADING THE PAGE.
   featureView.handleMainNav();
 };
 
