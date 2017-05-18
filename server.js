f4b6b1a92ee32fbb4e8c58a25e72e5bf4e1e4d5b
Scott <sdmccoy@gmail.com>
2017-05-17 21:42:20 -0700

    the server was not loading. I put in the static resource express as an argument correctly. It's now working but it's importing weird text strings as my features. Need to debug this

--- server.js
+++ server.js
@@ -4,14 +4,14 @@
 const express = require('express');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 //creating a constant to use for directing the port
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 3000;
 //instantiating the express package.
-var app = express();
+const app = express();
 //including the static resource express as an argument.
-app.use(express);
+app.use(express.static('.'));
 //new route that will handle a request and send a newFeature.html file back to the user.
 app.get('/newFeature', function (req, res) {
-  res.sendFile('/newFeature.html');
+  res.sendFile('newFeature.html', {root: '.'});
 });
 //this route will receive a new article from the form page and log that form data to the console.
 app.post('/articles', bodyParser, function (req, res){
