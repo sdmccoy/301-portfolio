be2d0174cd1c2335b7eb7d0a1ce72889157b079a
Scott <sdmccoy@gmail.com>
2017-05-24 14:50:27 -0700

    Created views dir and added all view files in it. Changed the paths in the index file. Added a repo view js file

---
+++ scripts/views/mainView.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = app || {};
+
+(function(module){
+
+  const featureView = {};
+
+  featureView.initIndexPage = function() {
+    app.Project.all.forEach(function(project){
+      $('#projects-content').append(project.toHtml());
+    });
+    // featureView.handleMainNav();
+  };
+  module.featureView = featureView;
+})(app);
+// TODO put in featureViewsetTeaser function to expand the descriptions
